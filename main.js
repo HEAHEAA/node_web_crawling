6ff7e91651502b7fb07489c72b898509b419f207
kimji <jhkim@youngjintech.com>
2024-03-13 16:57:00 +0900

    db연결

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const postgres = require('./db/postgresdb');
 
 
 const crawling = async () => {
-    console.log("크롤링 시작")
+    console.log("크롤링 시작");
+    
+    postgres.connect(function(err){
+        if(err) throw err;
+        console.log('pg db connect !')
+    });
 } 
 
 crawling();
