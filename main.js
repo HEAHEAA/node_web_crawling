50235565a338d5a23240adcb535d79b451f4348e
kimji <jhkim@youngjintech.com>
2024-03-13 16:28:34 +0900

    파일생성 01

---
+++ main.js
@@ -0,0 +1,9 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+
+const crawling = async () => {
+    console.log("크롤링 시작")
+} 
+
+crawling();
