eeddc36a85e9145132d9057e07335943cba9c4b2
kimji <jhkim@youngjintech.com>
2024-03-14 16:47:09 +0900

    크롤링 db insert 완료

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Tistory 블로그 :  https://kimjihee1113.tistory.com/9
 
-
+- db/postgresdb.js 은 개인정보로 숨김
--- main.js
+++ main.js
@@ -1,14 +1,60 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const sql = require('./db/sql');
 const postgres = require('./db/postgresdb');
+const nows = require('./util/date');
+
+
+
+
+
+
 
 const crawling = async () => {
-    console.log("크롤링 시작");
-    
-    postgres.connect(function(err){
-        if(err) throw err;
-        console.log('pg db connect !')
-    });
-} 
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://mw.genie.co.kr/newest');
+
+        const musicTitle = await driver.findElement(By.xpath('/html/body/section/ul[2]')).getText();
+        const replaceMusic = musicTitle.replace(/\n/g, ",");
+        const splitMusic = replaceMusic.split(',');
+
+        let title = [];
+        let name = [];
+        for (let i = 0; i < splitMusic.length; i++) {
+            //곡
+            if (i % 3 === 0) {
+                title.push(splitMusic[i]);
+            }
+            //가수
+            if (i % 3 === 1) {
+                name.push(splitMusic[i]);
+            }
+        }
+
+        for (let i = 0; i < title.length; i++) {
+            let value = [
+                title[i],
+                name[i],
+                nows + ' 00:00'
+            ]
+            postgres.query(sql.sql, value, function (err, row, fields) {
+                if (err) console.log(err);
+                console.log("Data Insert!!");
+            })
+
+        }
+
+
+
+
+
+    } catch (e) {
+        console.log(e);
+    } finally {
+        console.log('success')
+    }
+}
 
 crawling();
